6b7a36988a0debfe1951a83281e1b19f03e64303
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-09-26 20:13:43 -0600

    finished logout, login, and some more styling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,5 +29,28 @@ module.exports = {
         req.session.user = {email, name, userId: userId[0].user_id, isAdmin: false}
         // SEND THE session.user OBJECT TO THE FRONT END
         res.status(201).send({message: 'Logged in', user: req.session.user, loggedIn: true})
+    },
+    async login(req, res) {
+        const db = req.app.get('db')
+        const {email, password} = req.body
+
+        // CHECK IF USER EXISTS (and the hash)
+        const user = await db.find_user(email)
+        // if user doesn't exist, send appropriate response
+        if (!user[0]) return res.status(200).send({message: 'Email not found'})
+        // hash password and compare
+        const result = bcrypt.compareSync(password, user[0].hash)
+        // if hashes don't match, send appropriate response
+        if (!result) return res.status(200).send({message: 'Password incorrect'})
+        // if they do match, add user to sessions
+        /* isAdmin is set that way to destrucre is_admin off of user, AND THEN renaming it to isAdmin for js camel-casing */
+        const {name, is_admin:isAdmin, user_id:userId} = user[0]
+        req.session.user = {email, name, userId, isAdmin}
+        res.status(200).send({message: 'Logged in', user: req.session.user, loggedIn: true})
+        // send session.user back to front end
+    },
+    logout(req, res) {
+        req.session.destroy()
+        res.status(200).send({message: 'Logged out', loggedIn: false, user: null})
     }
 }
--- src/components/Catalog/Hero.jsx
+++ src/components/Catalog/Hero.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import './hero.css'
 import axios from 'axios'
 import swal from 'sweetalert2'
+import {updateUser} from '../../ducks/reducer'
+import {connect} from 'react-redux'
 
-export default class Hero extends Component {
+class Hero extends Component {
     constructor() {
         super()
         this.state = {
@@ -25,7 +27,9 @@ export default class Hero extends Component {
         const {email, name, password1, password2} = this.state
         if (password1 === password2) {
             const res = await axios.post('/auth/register', {email, name, password: password2})
-            console.log(res)
+            console.log(res.data)
+            this.props.updateUser(res.data.user)
+            swal.fire({type: 'success', text: res.data.message})
         } else {
             swal.fire({type: 'error', text: 'Passwords dont match'})
         }
@@ -68,3 +72,5 @@ export default class Hero extends Component {
         );
     }
 }
+
+export default connect(null, {updateUser})(Hero)
