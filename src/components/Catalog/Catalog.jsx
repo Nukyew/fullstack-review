014a147d06e7d7416474d7a213029f67dfe59810
Zachary Spilinek <zachary.spilinek@gmail.com>
2019-10-01 15:17:33 -0600

    more snipcart testing

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -32,7 +32,7 @@ export default class Catalog extends Component{
                 <button class="snipcart-add-item"
                     data-item-name="My Subscription"
                     data-item-id="subscription"
-                    data-item-url="www.thecarddrop.com/"
+                    data-item-url="https://thecarddrop.com/#/"
                     data-item-price="20.00"
                     data-item-payment-interval="Month"
                     data-item-payment-interval-count="2"
@@ -45,7 +45,7 @@ export default class Catalog extends Component{
                     data-item-name="Bacon"
                     data-item-price="3.00"
                     data-item-weight="20"
-                    data-item-url="https://thecarddrop.com/"
+                    data-item-url="https://thecarddrop.com/#/"
                     data-item-description="Some fresh bacon">
                     Buy bacon
                 </button>
